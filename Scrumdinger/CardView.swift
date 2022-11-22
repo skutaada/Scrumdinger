4b13e9296812ee45cb09ecf4a984a238a0f02a6f
Adam Skuta <adam.skuta@gmail.com>
2022-11-22 15:38:26 +0100

    Started project

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Adam Skuta on 22/11/2022.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20.0)
+            }
+            .font(/*@START_MENU_TOKEN@*/.caption/*@END_MENU_TOKEN@*/)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
