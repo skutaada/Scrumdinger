e00c5cb45d18c07fb38d0e27f1d133abb8d6a485
Adam Skuta <adam.skuta@gmail.com>
2022-11-22 13:31:17 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Adam Skuta on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
